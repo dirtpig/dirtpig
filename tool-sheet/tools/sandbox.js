e8fc44f25946cde486bf61fc7310df81b157c371
dirtpig <dirtpigtv@gmail.com>
2021-07-13 21:59:48 -0400

    learnin

---
+++ tool-sheet/tools/sandbox.js
@@ -0,0 +1,15 @@
+function checkAge(age) {
+    if (age >= 18) {
+      return true;
+    } else {
+      return confirm('Do you have permission from your parents?');
+    }
+  }
+  
+  let age = prompt('How old are you?', 18);
+  
+  if ( checkAge(age) ) {
+    alert( 'welcome to the cool kids club' );
+  } else {
+    alert( 'fuck outa here chump' );
+  }
