7d08aa6f6e030f229f4262c2b6ea0aeb94c16b8d
dirtpig <dirtpigtv@gmail.com>
2021-07-15 13:01:59 -0400

    learnin

---
+++ fundamentals-3/app.js
@@ -0,0 +1,15 @@
+
+function add7(num) {
+    if (num * 0 === 0) {
+        return num + 7;
+    }
+    else return "dafuck"
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+capitalize() {
+    
+}
