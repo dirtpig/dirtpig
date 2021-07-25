e40355e55b8faa82f0512ba5e32af215c2899395
dirtpig <dirtpigtv@gmail.com>
2021-07-25 18:09:03 -0400

    testing lets see how it goes
    commit

--- fundamentals-3/app.js
+++ fundamentals-3/app.js
@@ -1,4 +1,9 @@
 
+
+// fundamentals part 3 practice problems
+
+
+// 1. Write a function called add7 that takes one number and returns that number + 7.
 function add7(num) {
     if (num * 0 === 0) {
         return num + 7;
@@ -6,10 +11,44 @@ function add7(num) {
     else return "dafuck, that is not a number"
 }
 
+console.log(add7);
+
+
+
+// 2. Write a function called multiply that takes 2 numbers and returns their product.
 function multiply(a,b) {
     return a * b
 }
 
-capitalize() {
-    
+
+// 3. Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+
+
+
+function capitalize(yourString) {
+    let firstLetter = yourString.slice(0,1);
+    let uppeD = firstLetter.toUpperCase();
+    let theRest =
+  
+    return (yourString.toUpperCase(0,1)) + (yourString.toLowerCase(1));
 }
+
+
+
+
+
+
+
+
+
+
+// 4. Write a function called lastLetter that takes a string and returns the very last letter of that string
+// note: lastLetter("abcd") should return "d"
+
+
+
+
+
+
+
+
